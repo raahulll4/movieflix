477e5719571af6c6fc20c65a015c9ba3dca4e798
Rahul <rm.4120@outlook.com>
2023-09-20 16:08:35 +0100

    added data layer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ import MainComponent from "./Components/MainComponent/MainComponent";
 import { UserProvider } from "./Shared/js/user-context";
 
 function App() {
+  // window.adobeDataLayer = window.adobeDataLayer || [];
+  // window.adobeDataLayer.push({
+  //   "userInfo" : {
+  //     "id" : null,
+  //     "email" : null,
+  //     "firstName" : null,
+  //     "lastName" : null
+  //   },
+  // });
+  let script = document.createElement("script");
+  script.setAttribute("src", "adobe-client-data-layer.min.js")
+  script.async = true;
+  script.defer = true;
+  document.head.appendChild(script)
   return (
     <BrowserRouter>
       <div className="App">
